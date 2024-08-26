d869a8521e8f4dcc8d41a9930a162378b599e6a7
topia.amr <topia.amr@gmail.com>
2024-08-26 16:03:15 +0000

    update app

--- example/lib/index.dart
+++
@@ -1,6 +0,0 @@
-export 'package:flutter/material.dart';
-export 'package:lottie/lottie.dart';
-export 'package:m7_livelyness_detection/index.dart';
-
-export './main.dart';
-export './screens/index.dart';
